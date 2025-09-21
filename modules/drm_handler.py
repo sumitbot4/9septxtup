b1dbcb5f86b7a82d4744cabc78aea1991486ee8d
sumitbot4 <870.sumit123@gmail.com>
2025-09-21 08:40:26 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -288,7 +288,7 @@ async def drm_handler(bot: Client, m: Message):
          
             elif "https://cpvod.testbook.com/" in url or "classplusapp.com/drm/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
-                url = f"https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}"
+                url = f"https://marco-cp-api.vercel.app/marco?url={url}@ITSMARCOOFFICIAL&user_id={user_id}"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
